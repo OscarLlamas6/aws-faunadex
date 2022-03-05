3c16962b5b411263bd99aea3806de290ed65df52
201709309 <josesantizo149@gmail.com>
2022-03-04 23:02:22 -0600

    front con nodejs ya esta

--- front-faunadex/src/pages/EditProfile.js
+++ front-faunadex/src/pages/EditProfile.js
@@ -1,6 +1,6 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "../css/style.css"
-import {Button,Form} from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import http from '../libs/http'
 
 var reader = new FileReader();
@@ -13,15 +13,24 @@ class EditProfile extends Component {
         password: ""
     }
 
-    componentDidMount(){
+    componentDidMount() {
         if (!window.localStorage.getItem("iduser")) {
-            window.location.href="./"
+            window.location.href = "./"
         }
+
+        this.llenarEstado()
     }
 
-    handleChange=async e=>{
+    llenarEstado = async () => {
+        this.setState({
+            user: window.localStorage.getItem("username"),
+            name: window.localStorage.getItem("nombre"),
+        })
+    }
+
+    handleChange = async e => {
         await this.setState({
-            ...this.state,[e.target.name]: e.target.value
+            ...this.state, [e.target.name]: e.target.value
         });
     }
 
@@ -34,19 +43,21 @@ class EditProfile extends Component {
         };
     }
 
-    EditUser=async()=>{
+    EditUser = async () => {
         if (this.state.password === window.localStorage.getItem("password")) {
             let userid = window.localStorage.getItem("iduser");
-             //console.log(window.localStorage.getItem("iduser"),this.state.user,this.state.name,this.state.password,reader.result.split(",")[1]);
-            let req = await http.put('http://localhost:4000/usuario/updateUsuario',{
+            //console.log(window.localStorage.getItem("iduser"),this.state.user,this.state.name,this.state.password,reader.result.split(",")[1]);
+            let req = await http.put('http://localhost:4000/usuario/updateUsuario', {
                 usuarioId: userid,
                 userName: this.state.user,
                 nombre: this.state.name,
                 password: this.state.password
             })
             if (req.error) {
-                alert("Error al actualizar")
+                alert(req.message)
             } else {
+                window.localStorage.setItem("nombre", req.result.nombre)
+                window.localStorage.setItem("username", req.result.userName)
                 alert("Actualizado correctamente")
             }
             console.log(req)
@@ -55,30 +66,48 @@ class EditProfile extends Component {
         }
     }
 
+    EditFotoPerfil = async () => {
+            let userid = window.localStorage.getItem("iduser");
+            let req = await http.put('http://localhost:4000/usuario/updateFotoPerfil', {
+                usuarioId: userid,
+                linkFotoPerfil: reader.result.split(",")[1]
+            })
+            if (req.error) {
+                alert(req.message)
+            } else {
+                window.localStorage.setItem("pfp",req.result)
+                alert("Actualizado correctamente")
+            }
+    }
+
     render() {
         return (
             <div className="Main">
                 <h1>Editar Perfil</h1>
                 <div>
-                <Form>
-                    <Form.Group>
-                        <Form.Label>Usuario: </Form.Label>
-                        <Form.Control type="text" placeholder="Usuario" name="user" onChange={this.handleChange}/>
-                        <Form.Label>Nombre Completo: </Form.Label>
-                        <Form.Control type="text" placeholder="Nombre" name="name" onChange={this.handleChange}/>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Usuario: </Form.Label>
+                            <Form.Control type="text" placeholder="Usuario" defaultValue={this.state.user} name="user" onChange={this.handleChange} />
+                            <Form.Label>Nombre Completo: </Form.Label>
+                            <Form.Control type="text" placeholder="Nombre" defaultValue={this.state.name} name="name" onChange={this.handleChange} />
+                            <Form.Label>Elegir Foto</Form.Label>
+                            <Form.Control type="file" onChange={this.handleImage} accept=".jpg,.png" />
+                            <Form.Label>Password: </Form.Label>
+                            <Form.Control type="password" placeholder="Password" name="password" onChange={this.handleChange} />
+                        </Form.Group>
+                        <br />
+                        <Button variant="primary" onClick={() => this.EditUser()}>Guardar</Button>
+                        <br />
                         <Form.Label>Elegir Foto</Form.Label>
-                        <Form.Control type="file" onChange={this.handleImage} accept=".jpg,.png"/>
-                        <Form.Label>Password: </Form.Label>
-                        <Form.Control type="password" placeholder="Password" name="password" onChange={this.handleChange}/>
-                    </Form.Group>
-                    <br/>
-                    <Button variant="primary" onClick={()=>this.EditUser()}>Guardar</Button>
-                    <br/>
-                    <br/>
-                    <Button variant="primary" href="/home">Regresar</Button>
-                </Form>
+                        <Form.Control type="file" onChange={this.handleImage} accept=".jpg,.png" />
+                        <Button variant="primary" onClick={() => this.EditFotoPerfil()}>Cambiar Foto</Button>
+                        <br />
+                        <br />
+                        <Button variant="primary" href="/home">Regresar</Button>
+                    </Form>
                 </div>
-            </div> 
+            </div>
         )
     }
 }
